967f26ec3382a964adcb2e25dbe1471f4c25686a
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2024-06-16 18:15:20 +0000

    unused sanity schemas removed.

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import blockContent from './schemaTypes/blockContent'
-import category from './schemaTypes/category'
 import post from './schemaTypes/post'
-import author from './schemaTypes/author'
-import language from './schemaTypes/language'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, language, blockContent],
+  types: [post, blockContent],
 }
