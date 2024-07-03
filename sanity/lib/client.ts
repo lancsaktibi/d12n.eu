25a748640a104d385dde80b689870051ec6dbbac
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2024-07-03 20:15:37 +0000

    Query files adjusted.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,14 @@
-import { createClient } from 'next-sanity'
+import { createClient, type SanityClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
-export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+export function getClient():SanityClient {
+  const client = createClient({
+    apiVersion,
+    dataset,
+    projectId,
+    useCdn,
+  })
+  return client
+} 
+
