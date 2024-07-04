cc1879c81c71ee676025807c957f4ada17acb09c
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2024-07-04 04:52:21 +0200

    Aktualisieren von page.tsx

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,31 +1,15 @@
-import type { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { PortableText} from "@portabletext/react"
-import Image from 'next/image'
-import { getClient } from "../../sanity/lib/client"
-import { urlForImage } from "../../sanity/lib/image"
-import {
-  getStart,
-  type Post,
-  startQuery
-} from '../../sanity/lib/queries'
-import { useLiveQuery } from 'next-sanity/preview'
-import { start } from 'repl'
+import { SanityDocument } from "@sanity/client";
+import { startQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/sanityFetch";
 
-interface Query {
-  [key: string]: string
-}
+const Page = async () => {
+    const post = await sanityFetch<SanityDocument>({
+        query: startQuery,
+    });
 
-async function getStartPage() {
-  const client = getClient()
-  const start = await getStart(client)
- 
-  return start
-}
- 
-export default async function TestPage() {
-  const startPage = await getStartPage{}
-  return (
-    <div className="post__content">
-    <PortableText value={startPage.body} />
-  </div>
-  )
+    return (
+            <div>{post.body}</div>
+    );
+};
+
+export default Page;
