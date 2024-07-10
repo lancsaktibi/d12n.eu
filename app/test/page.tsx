2ecb299a6ebc0d00949b30fce041de196ecbfd53
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2024-07-10 14:52:43 +0000

    tailwind css blog section added.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -9,10 +9,20 @@ const Page = async () => {
     });
 
     return (
-            <div>                
-                <PortableText value={post.body} />
+        <div className="flex justify-center">
+            <div className="bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            {post.title}
+                        </h2>
+                        <div className="mt-2 text-lg leading-8 text-gray-600">
+                            <PortableText value={post.body} />
+                        </div>
+                    </div>
+                </div>
             </div>
-
+        </div>
     );
 };
 
