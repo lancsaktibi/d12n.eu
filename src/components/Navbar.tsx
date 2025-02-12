1625c8f3af4ffcff13a01b0a00d09961d3f75666
Tibor Lancsak <40690847+lancsaktibi@users.noreply.github.com>
2025-02-12 15:18:42 +0000

    Navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,46 +4,41 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 left-0 z-50 w-full bg-gray-800 text-white md:px-8 py-4">
-            <div className="w-full flex justify-between items-center md:px-8 py-2">
+      <nav className="sticky top-0 left-0 z-50 w-full bg-gray-800 text-white md:px-8 py-4">
+        <div className="w-full flex justify-between items-center md:px-8 py-2">
         
-                {/**
-                *    {/* Left Section with 3 Links 
-                *    <section className="flex space-x-4">
-                *    <Link href="/link1" className="hover:underline">
-                *    Link 1
-                *    </Link>
-                *    <Link href="/link2" className="hover:underline">
-                *    Link 2
-                *    </Link>
-                *    <Link href="/link3" className="hover:underline">
-                *    Link 3
-                *    </Link>
-                *    </section> 
-                 */}
-
+          {/* Left Section with 3 Links */}
+          <section className="flex space-x-4">
+            <Link href="/" className="hover:underline">
+              politische Bildung
+            </Link>
+            <Link href="/" className="hover:underline">
+              politische Theorie 
+            </Link>
+            <Link href="/link3" className="hover:underline">
+              Kommunikationstheorie
+            </Link>
+          </section> 
   
-          {/* Middle Section with Icon */}
+          {/* Middle Section with Title */}
           <section className="flex justify-center">
             <Link href="/" className="text-lg font-semibold hover:underline">
               D12N.EU
             </Link>
           </section>
-  
-          {/**
-            * 
-            *           {/* Right Section with 3 Links 
-            * <section className="flex space-x-4">
-            *  <Link href="/link4" className="hover:underline">
-            *   Link 4
-            * </Link>
-            * <Link href="/link5" className="hover:underline">
-    	    * </Link>
-            * <Link href="/link6" className="hover:underline">
-            *   Link 6
-            * </Link>
-            * </section>
-           */}
+
+          {/* Right Section with 3 Links */}
+          <section className="flex space-x-4">
+            <Link href="/" className="hover:underline">
+              DE
+            </Link>
+            <Link href="/" className="hover:underline">
+              EN 
+            </Link>
+            <Link href="/link3" className="hover:underline">
+              HU
+            </Link>
+          </section>   
         </div>
       </nav>
     );
